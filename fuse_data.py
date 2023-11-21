aa23c1a96b96ad5a5c55da25e929b154d48af4ba
throgg <ghus266@gmail.com>
2023-11-21 14:18:28 +0100

    added tqdm & loops through files

--- fuse_data.py
+++ fuse_data.py
@@ -4,24 +4,23 @@ import pandas as pd
 import numpy as np
 import os 
 import open3d as od
+from tqdm import tqdm
 
 # Listing all the files in mensura directory and distance from start
 path = os.path.join("mensura/")
-for file in os.listdir(path): 
+for file in tqdm(os.listdir(path)): 
     distance = 20 * (int(file.split(".")[0]) -1) 
-    print(f"{file} @ {distance}")
-
-# open .ply file and save distances to numpy array
-# TODO: add following code to loop
-input_file = os.path.join("mensura/1.ply")
-file = open(input_file, 'r')
-coordinates = []
-for (num, line) in enumerate(file): 
-    if (num > 13) and (len(line.split())<=3) and (num%2 != 0):
-        x, y, z = line.split()
-        coordinates.append([x, y, z])
-# turning coordinates list in np array
-np_coordinates = np.array(coordinates)
+    # open .ply file and save distances to numpy array
+    # TODO: add following code to loop
+    input_file = os.path.join(f"mensura/{file}")
+    file = open(input_file, 'r')
+    coordinates = []
+    for (num, line) in enumerate(file): 
+        if (num > 13) and (len(line.split())<=3) and (num%2 != 0):
+            x, y, z = line.split()
+            coordinates.append([x, y, z])
+    # turning coordinates list in np array
+    np_coordinates = np.array(coordinates)
 
 
 
